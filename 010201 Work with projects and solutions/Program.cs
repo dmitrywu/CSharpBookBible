ef35d5c114e97b7590ee15c418813b5d4b1ec5a2
Dmitry <dmitry.wu@gmail.com>
2025-03-31 15:42:08 +0300

    add Arrays samples

---
+++ 010201 Work with projects and solutions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _010201_Work_with_projects_and_solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Text");
+            Console.WriteLine($"Text");
+            
+            string s;
+            s = "Console.ReadLine()";
+            Console.WriteLine($"Entered text: {s}");
+            Console.WriteLine(s);
+            
+            int a, b, c;
+
+            string str = "Это строка";
+            str = "Это ещё одна строка";
+            Console.WriteLine(str);
+
+            char ch = 'c';
+            Console.WriteLine(ch);
+
+            int[] intArray;
+            intArray = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        }
+    }
+}
---
+++ 020402 Arrays/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] weekDays = {"Понедельник","Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine(weekDays[new Random().Next(weekDays.Length)]);
+
+int[,] myArray = new int[3, 3];
+
+
